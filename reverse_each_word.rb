d51fe71359150556f6fba17f48d5a6ac8937d97b
Victor Torres <torres.victor136@gmail.com>
2020-08-27 22:22:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_world(sentence)
   rsentence = []
-  sentence.split.collect do |world|
+  sentence.split.collect do |worsd|
     rsentence << word.reverse 
   end 
   reverse.join(" ")
